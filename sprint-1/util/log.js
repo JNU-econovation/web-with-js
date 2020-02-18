c53f376eda32a72c370268216e788297c832d110
ummaeha <raya000@naver.com>
2020-02-18 15:24:02 +0900

    1주차

--- sprint-1/util/log.js
+++ sprint-1/util/log.js
@@ -1,5 +1,25 @@
+// let TimeConverter = require("./domain/time-converter");
+
 class Log {
-  constructor() {}
+  constructor(array) {
+    this.d = array[0];
+    this.h = array[1];
+    this.m = array[2];
+    this.s = array[3];
+  }
+
+  printMS() {
+    console.log(this.m + "m" + this.s + "s");
+  }
+  print() {
+    console.log(this.h + "h" + this.m + "m" + this.s + "s");
+  }
+  printH() {
+    console.log(this.h + "h");
+  }
+  printHMS() {
+    console.log(this.h + "h" + this.m + "m" + this.s + "s");
+  }
 }
 
-module.exports = TimeConverter;
+module.exports = Log;
