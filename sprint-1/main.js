0b352c9579b0264d42df7602f0467dfb624f9e3b
kcy13930 <kcy13930@gmail.com>
2020-02-18 13:52:46 +0900

    timeconverter_01

--- sprint-1/main.js
+++ sprint-1/main.js
@@ -1,3 +1,13 @@
-function main() {}
+function main() {
+    const TimeConverter = require('./domain/time-converter.js');
+    const Log = require('./util/log.js');
+
+    let test = new TimeConverter("3837s h");
+    test.isType();
+    test.cvt();
+    let util = new Log(test.n_data);
+
+    util.printData();
+}
 
 main();
