49b41fad7044a376b999b703557764d86a026568
vvvpiano <vvvpiano60@gmail.com>
2020-02-24 20:03:23 +0900

    array.js에서 command 구현해놓음

--- sprint-2/main.js
+++ sprint-2/main.js
@@ -1,4 +1,7 @@
 const Commander = require("./domain/commander");
 const Log = require("./util/log");
-function main() {}
+function main() {
+    const input = ["count 1 2 3 4 5", "count 1 '하나' 2 '둘' 3 '셋' 4", "count one two three four five", "add 35 23", "add 100 five"];
+
+}
 main();
