43b38b763c63a0958c44551ebc6f54ad44616cf1
vvvpiano <vvvpiano60@gmail.com>
2020-03-26 23:42:01 +0900

    기능구현 완료

---
+++ sprint-5/domain/game369.js
@@ -0,0 +1,86 @@
+const EventEmitter = require('events');
+
+class Game369 extends EventEmitter {
+    constructor() {
+        super();
+        this.participants = []; // person들을 모아서 받은 배열
+
+        this.on('start', (starter_name) => {
+            console.log(">>> Game Start <<<")
+            this.startGame(starter_name);
+        })
+
+        this.on('end', () => {
+            console.log(">>> Game Over <<<");
+        })
+    }
+
+    startGame(starter_name) {
+        if (this.participants.length <= 0)
+            throw new Error("participants가 없습니다");
+        let number = 1;
+        const starter = starter_name;
+        this.emit(starter, number, this.participants);
+    }
+
+    progressGame(name, number) {
+        if (this.is369(number)) { // 박수를 쳐야 할 때
+            const index = this.findIndex(name);
+            if (this.clapOrNot(this.participants[index].accuracy)) {
+                this.doAction(name, "clap!");
+                this.emit(this.findNext(name), ++number);
+            }
+            else {
+                this.doAction(name, number);
+                console.log(`${name}이(가) 게임에서 졌습니다`);
+                this.emit("end");
+            }
+        } else
+            this.play(name, number);
+    }
+
+    is369(number) {
+        let num = number;
+        let digit;
+        while (num > 0) {
+            digit = num % 10;
+            if (digit === 3 || digit === 6 || digit === 9)
+                return true;
+            num = parseInt(num / 10);
+        }
+        return false;
+    }
+
+    clapOrNot(accuracy) {
+        let rand = Math.random();
+        if (rand < accuracy)
+            return true;
+        return false;
+    }
+
+    play(name, number) {
+        this.doAction(name, number);
+        let next = this.findNext(name)
+        this.emit(next, ++number);
+    }
+
+    doAction(name, action) {
+        console.log(`${name}: ${action}`);
+    }
+
+    findNext(name) {
+        let index = this.findIndex(name);
+        const next_index = (++index) % this.participants.length;
+        return this.participants[next_index].name;
+    }
+
+    findIndex(name) {
+        for (let i = 0; i < this.participants.length; i++) {
+            if (this.participants[i].name === name)
+                return i;
+        }
+        return new Error("인덱스를 찾지 못함");
+    }
+}
+
+module.exports = Game369;
---
+++ sprint-5/main.js
@@ -0,0 +1,34 @@
+const Person = require('./domain/person');
+const Game369 = require('./domain/game369');
+
+function main() {
+    console.log("<< A와 B가 게임. accuracy 기본값 0.5 >>\n");
+    const personA = new Person('A');
+    const personB = new Person('B');
+
+    const game369 = new Game369();
+
+    personA.enterGame(game369);
+    personB.enterGame(game369);
+    personA.startGame();
+
+    console.log("\n");
+
+    console.log("<< A와 B와 C가 게임. accuracy 각각 0.85, 0.78, 0.99 >>");
+    console.log("게임은 B가 시작");
+
+    const personC = new Person("C");
+    personC.enterGame(game369);
+    personA.setAccuracy(0.85);
+    personB.setAccuracy(0.78);
+    personC.setAccuracy(0.99);
+    personB.startGame();
+
+    // console.log(personA);
+    // console.log(personB);
+
+
+
+}
+
+main();
