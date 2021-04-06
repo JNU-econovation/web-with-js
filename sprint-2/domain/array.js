c54445ef894464ef1dc0295b3ad8c443c21982c9
vvvpiano <vvvpiano60@gmail.com>
2021-04-06 15:52:45 +0900

    refactor: sprint-2

--- sprint-2/domain/array.js
+++ sprint-2/domain/array.js
@@ -1,4 +1,4 @@
-Array.prototype.customReduce = function (callback, initialValue = 0) {
+Array.prototype.customReduce = function (callback, initialValue) {
   const src = Object(this);
   const length = src.length;
   let index = 0;
@@ -8,11 +8,14 @@ Array.prototype.customReduce = function (callback, initialValue = 0) {
   /*
     실제 자바스크립트 표준객체 Array의 reduce에 대한 설명을 참고했을 때 initialValue가 있으면
     그 initialValue 위에 reduce를 해나가고(accumulator = initialValue), 
-    initialValue가 없으면 acculator가 0으로 초기화된 상태 그대로 그 위에 reduce를 합니다.
+    initialValue가 없으면 acculator가 array의 첫번째 element로 초기화된 뒤 그대로 그 위에 reduce를 합니다.
   */
 
   if (!initialValue && length === 0)
     throw new Error("에러: reduce할 값이 없습니다");
+
+  if (!accumulator)
+    accumulator = this[index++];
 
   while (index < length) {
     currentValue = this[index++];
