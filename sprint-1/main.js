e3d37b2f4e269eee6c7c8e2595d0df80c4ed71cb
spiderq2697 <spiderq10@gmail.com>
2022-02-16 17:41:44 +0900

    refactor : 기본 main.js 수정

--- sprint-1/main.js
+++ sprint-1/main.js
@@ -1,5 +1,5 @@
-class Log {
-    constructor() {}
-  }
-  
-  module.exports = TimeConverter;  
+const TimeConverter = require("./domain/time-converter.js");
+const Log = require("./util/log.js");
+
+// 입출력 예제를 참고하여 바꾸셔도 됩니다.
+const input = "1600s m"
