e2e2f528f3f2e947725b01027da997335b0d0966
2yunseong <dbsdltjd3701@naver.com>
2022-02-17 08:12:10 +0900

    feat : parser 생성

---
+++ sprint-1/domain/parser.js
@@ -0,0 +1,17 @@
+class Parser {
+  constructor() {
+    this.baseUnit = "";
+    this.convertTargetTime = "";
+  }
+
+  parseCommand(command) {
+    try {
+    } catch (error) {}
+  }
+
+  parseBaseUnit() {}
+
+  parseConvertTargetTime() {}
+}
+
+module.exports = Parser;
