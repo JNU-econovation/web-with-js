7e74ecf3af7f38628c00e927e4e92ecb650bc6e1
2yunseong <dbsdltjd3701@naver.com>
2022-02-17 09:29:28 +0900

    feat : 명령어에서, 허용되는 단어만 사용하였는지 체크하는 로직 작성

--- sprint-1/domain/parser.js
+++ sprint-1/domain/parser.js
@@ -4,9 +4,30 @@ class Parser {
     this.convertTargetTime = "";
   }
 
-  parseCommand(command) {
+  checkUsingVaildWord(command) {
+    const vaildCharList = ["s", "d", "m", "h", " "];
+    for (let i = 0; i < 10; i++) {
+      vaildCharList.push(i + "");
+    }
+
+    try {
+      for (const element of command) {
+        if (vaildCharList.includes(element) === false) {
+          throw new Error("에러: 허용하지 않는 단어가 존재합니다.");
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  checkReliableInput(command) {
     try {
     } catch (error) {}
+  }
+
+  parseCommand(command) {
+    this.checkReliableInput(command);
   }
 
   parseBaseUnit() {}
