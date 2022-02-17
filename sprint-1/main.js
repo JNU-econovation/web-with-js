b584a1b9163fa3b2f4106b91fab8e2a941ad9ad7
spiderq2697 <spiderq10@gmail.com>
2022-02-17 09:43:02 +0900

    feat : timeConverter, log 인스턴스 생성, 결과값 출력

--- sprint-1/main.js
+++ sprint-1/main.js
@@ -2,4 +2,10 @@ const TimeConverter = require("./domain/time-converter.js");
 const Log = require("./util/log.js");
 
 // 입출력 예제를 참고하여 바꾸셔도 됩니다.
-const input = "1600s m"
+const input = "1600s m";
+const [beforeConvert, afterConvertUnit] = input.split(" ");
+
+timeConverter = new TimeConverter(beforeConvert);
+log = new Log(timeConverter.converter(), afterConvertUnit);
+
+log.print();
