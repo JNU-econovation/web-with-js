7113fb52caa19691a0247cf4b1e5943d6bd16a36
spiderq2697 <spiderq10@gmail.com>
2022-02-17 09:42:11 +0900

    feat : converToSeconds 메소드, converToAfterUnit 생성 convert된 객체를 받아서 second로 변환하는 converToSeconds 메소드, 그 결과 값으로 목표 단위의 값을 만들어주는 converToAfterUnit 생성

--- sprint-1/util/log.js
+++ sprint-1/util/log.js
@@ -1,5 +1,37 @@
 class Log {
-  constructor() {}
+  #result = "";
+  #totalSeconds = 0;
+  #timeKeys = ["d", "h", "m", "s"];
+  #multiplyValues = [86400, 3600, 60, 1];
+
+  constructor(converted, afterConvertUnit) {
+    this.converted = converted;
+    this.afterConvertUnit = afterConvertUnit;
+  }
+
+  convertToSeconds() {
+    for (let [index, key] of this.#timeKeys.entries()) {
+      if (key in this.converted) {
+        this.#totalSeconds += this.converted[key] * this.#multiplyValues[index];
+      }
+    }
+  }
+
+  convertToAfterUnit() {
+    for (let [index, key] of this.#timeKeys.entries()) {
+      if (this.convertToAfterUnit === key) {
+        this.#result += this.#totalSeconds / this.#multiplyValues[index];
+        this.#result += key;
+        this.#totalSeconds % this.#multiplyValues[index];
+      }
+    }
+  }
+
+  print() {
+    this.convertToSeconds();
+    this.convertToAfterUnit();
+    console.log(this.#result);
+  }
 }
 
 module.exports = Log;
