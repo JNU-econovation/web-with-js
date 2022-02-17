05687f6f47744b86b48f5158380bb9c5757a2eb6
spiderq2697 <spiderq10@gmail.com>
2022-02-17 09:20:40 +0900

    feat : 입력받은 값을 convert해주는 TimeConverter 생성 입력받은 값을 d, h, m, s 각각의 문자를 기준으로 잘르고 각 문자를 key로, 각 문자에 해당하는 숫자를 value로 갖는 Object를 반환하는 converter 메소드 생성

--- sprint-1/domain/time-converter.js
+++ sprint-1/domain/time-converter.js
@@ -1,5 +1,23 @@
 class TimeConverter {
-  constructor() {}
+  #result = {};
+
+  constructor(beforeConvert) {
+    this.beforeConvert = beforeConvert;
+  }
+
+  converter() {
+    let afterSplit = [];
+    const timeKeys = ["d", "h", "m", "s"];
+
+    for (let key of timeKeys) {
+      if (this.beforeConvert.indexOf(key) != -1) {
+        afterSplit = this.beforeConvert.split(key);
+        this.beforeConvert = afterSplit[1];
+        this.#result[key] = Number(afterSplit[0]);
+      }
+    }
+    return this.#result;
+  }
 }
 
 module.exports = TimeConverter;
