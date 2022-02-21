9269290ee65ffde18f07ac8930b6e74a8e34202a
2yunseong <dbsdltjd3701@naver.com>
2022-02-21 11:06:41 +0900

    feat : 에러 체크에 필요한 메서드 구현

--- sprint-1/domain/parser.js
+++ sprint-1/domain/parser.js
@@ -1,19 +1,22 @@
+const ERROR_INVALID_WORD_MESSAGE = "에러: 허용하지 않는 단어가 존재합니다.";
+const ERROR_INVALID_INPUT_MESSAGE = "에러 : 입력이 올바르지 않습니다.";
+
 class Parser {
   constructor() {
     this.baseUnit = "";
     this.convertTargetTime = "";
   }
 
-  checkUsingVaildWord(command) {
-    const vaildCharList = ["s", "d", "m", "h", " "];
+  checkUsingValidWord(command) {
+    const validCharList = ["s", "d", "m", "h", " "];
     for (let i = 0; i < 10; i++) {
-      vaildCharList.push(i + "");
+      validCharList.push(i + "");
     }
 
     try {
       for (const element of command) {
-        if (vaildCharList.includes(element) === false) {
-          throw new Error("에러: 허용하지 않는 단어가 존재합니다.");
+        if (validCharList.includes(element) === false) {
+          throw new Error(ERROR_INVALID_WORD_MESSAGE);
         }
       }
     } catch (error) {
@@ -23,16 +26,27 @@ class Parser {
 
   checkReliableInput(command) {
     try {
-    } catch (error) {}
+      const splitCommand = command.split();
+      if (!this.isSplitCommandValid(splitCommand)) {
+        throw new Error(ERROR_INVALID_INPUT_MESSAGE);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  isSplitCommandValid(splitCommand) {
+    return !(splitCommand.length != 2 || splitCommand[1].length != 1);
   }
 
   parseCommand(command) {
     this.checkReliableInput(command);
+    this.checkUsingValidWord(command);
+
+    const splitCommand = command.split();
+    this.convertTargetTime = splitCommand[0];
+    this.baseUnit = splitCommand[1];
   }
-
-  parseBaseUnit() {}
-
-  parseConvertTargetTime() {}
 }
 
 module.exports = Parser;
