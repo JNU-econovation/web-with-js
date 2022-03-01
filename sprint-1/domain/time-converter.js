5a583bd9378184c536262057b1ae95c4b92b8d76
singco <65025333+spiderq2697@users.noreply.github.com>
2022-03-02 00:05:00 +0900

    Revert "[스프린트 1주차] 미션 제출합니다."

--- sprint-1/domain/time-converter.js
+++ sprint-1/domain/time-converter.js
@@ -1,28 +1,5 @@
 class TimeConverter {
-  #result = {};
-
-  constructor(beforeConvertTime) {
-    this.beforeConvertTime = beforeConvertTime;
-  }
-
-  splitByUnit(unit) {
-    let afterSplit = [];
-
-    if (this.beforeConvertTime.indexOf(unit) != -1) {
-      afterSplit = this.beforeConvertTime.split(unit);
-      this.beforeConvertTime = afterSplit[1];
-      this.#result[unit] = Number(afterSplit[0]);
-    }
-  }
-
-  converter() {
-    const timeUnits = ["d", "h", "m", "s"];
-
-    for (let key of timeUnits) {
-      this.splitByUnit(key);
-    }
-    return this.#result;
-  }
+  constructor() {}
 }
 
 module.exports = TimeConverter;
